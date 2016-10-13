50f8f03c6f41aba1c1b50e29ce19cf8c5fb1643c
scorched <scorched@bk.ru>
2016-10-14 00:01:30 +0300

    Show what state is active when starting a new project.

--- default/source/MenuState.hx
+++ default/source/MenuState.hx
@@ -9,9 +9,14 @@ import flixel.math.FlxMath;
 
 class MenuState extends FlxState
 {
+	var label:FlxText;
+	
 	override public function create():Void
 	{
 		super.create();
+		
+		label = new FlxText(10, 10, 500, "MenuState", 36);
+		add(label);
 	}
 
 	override public function update(elapsed:Float):Void
--- default/source/PlayState.hx
+++ default/source/PlayState.hx
@@ -9,9 +9,14 @@ import flixel.math.FlxMath;
 
 class PlayState extends FlxState
 {
+	var label:FlxText;
+	
 	override public function create():Void
 	{
 		super.create();
+		
+		label = new FlxText(10, 10, 500, "PlayState", 36);
+		add(label);
 	}
 
 	override public function update(elapsed:Float):Void
